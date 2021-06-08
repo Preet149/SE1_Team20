904484021cc18077ff1f4695b520487735021ab9
Preet149 <82959745+Preet149@users.noreply.github.com>
2021-06-08 18:37:28 +0200

    Implemented functional programming in the main-method

--- src/Exercise7/Main.java
+++ src/Exercise7/Main.java
@@ -2,23 +2,30 @@ package Exercise7;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[]args){
 
         Auto a1=new Auto();
 
-        a1.hatTurbo();
-        a1.kunde().isFirma();
-
         Auto a2=new Auto();
 
-        a1.hatTurbo();
-        a1.kunde().isFirma();
+        Auto a3=new Auto();
 
-        List<Auto>auto= Arrays.asList(a1,a2);
+        Auto a4=new Auto();
+
+        Auto a5=new Auto();
+
+        List<Auto> autos= Arrays.asList(a1,a2,a3,a4,a5);
+
+        List<Auto> firmen= Arrays.asList();
+
+        System.out.println(autos.stream().filter(x -> x.hatTurbo() && x.kunde().isFirma()).map(x -> x.parkdauer()* new ParkHaus().gebühr()).reduce(0,Integer::sum)+" Euro");
+
+        System.out.println(autos.stream().filter(x -> x.parkdauer()>1440).map(x ->firmen.add(x) ));
+
+
 
     }
 }
