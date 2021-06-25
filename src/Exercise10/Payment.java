62367999cb97618a3113f3110edfdd1d2829f401
Preet149 <82959745+Preet149@users.noreply.github.com>
2021-06-25 23:00:38 +0200

    Added 3 Instanzvariable and a constructor into the class Payment.

--- src/Exercise10/Payment.java
+++ src/Exercise10/Payment.java
@@ -1,4 +1,43 @@
 package Exercise10;
 
+import java.util.regex.Pattern;
+
 public class Payment {
+
+    private long kontonummer;
+    private String iban;
+    private long blz;
+
+    public Payment(int kontonummer,String iban,int blz)throws IllegalArgumentException{
+
+        if(Pattern.matches("([0-9]{10})","kontonummer")){
+
+             this.kontonummer=kontonummer;
+        }
+        else{
+
+            throw new IllegalArgumentException();
+        }
+
+        if(Pattern.matches("([A-Z]{2}[0-9]{20})","iban")){
+
+            this.iban=iban;
+        }
+        else{
+
+            throw new IllegalArgumentException();
+        }
+
+        if(Pattern.matches("([0-9]{8})","blz")){
+
+            this.blz=blz;
+        }
+        else{
+
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+
 }
